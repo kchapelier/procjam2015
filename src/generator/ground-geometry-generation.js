4836cb2b8fe6627c665a0ae8d63677a0af5d342d
kchapelier <kevin.chapelier@gmail.com>
2017-03-03 17:23:49 +0100

    remove zeros/ndarray dependencies in ground-geometry-generation, optimizations

--- src/generator/ground-geometry-generation.js
+++ src/generator/ground-geometry-generation.js
@@ -1,55 +1,55 @@
-var zeros = require('zeros');
+//TODO remove threejs dependency
+//TODO fix normal seams at the edges
 
 var THREE = require('three');
 
 var prepareBufferGeometry = function prepareBufferGeometry (data, segmentSize, groundSegments) {
-    var groundGeometry = new THREE.PlaneGeometry(groundSegments * segmentSize, groundSegments * segmentSize, groundSegments, groundSegments);
+    var width = groundSegments + 1,
+        groundGeometry = new THREE.PlaneGeometry(groundSegments * segmentSize, groundSegments * segmentSize, groundSegments, groundSegments);
 
     groundGeometry.rotateX(-Math.PI / 2);
 
-    for (var x = 0; x <= groundSegments; x++) {
-        for (var z = 0; z <= groundSegments; z++) {
-            var i = x * (groundSegments + 1) + z;
+    for (var x = 0; x < width; x++) {
+        for (var z = 0; z < width; z++) {
+            var i = x + z * width;
 
-            groundGeometry.vertices[i].y = data[z * (groundSegments + 1) + x];
+            groundGeometry.vertices[i].y = data[i];
         }
     }
 
-    groundGeometry.computeFaceNormals();
     groundGeometry.computeVertexNormals();
-    groundGeometry.normalsNeedUpdate = true;
 
     return new THREE.BufferGeometry().fromGeometry(groundGeometry);
 };
 
 var groundGeometryData = function groundGeometryData (rng, offsetX, offsetZ, chunkSize, groundSegments) {
     var segmentSize = chunkSize / groundSegments,
-        ndarrayMap2 = zeros([groundSegments + 1, groundSegments + 1]),
+        ratioGeneration = segmentSize / 100, // the algorithm was originally written for segmentSize of 100, we don't want the result to be dramatically different depending on the segmentSize
+        width = groundSegments + 1,
+        typedArray = new Float32Array(width * width),
         x,
         y,
         z,
         dist,
         offsettedX, offsettedZ;
 
-    var ratioGeneration = segmentSize / 100; // the algorithm was originally written for segmentSize of 100, we don't want the result to be dramatically different depending on the segmentSize
-
     offsetX = offsetX * groundSegments;
     offsetZ = offsetZ * groundSegments;
 
-    for (x = 0; x < ndarrayMap2.shape[0]; x++) {
+    for (x = 0; x < width; x++) {
         offsettedX = (x + offsetX) * ratioGeneration;
-        for (z = 0; z < ndarrayMap2.shape[1]; z++) {
+        for (z = 0; z < width; z++) {
             offsettedZ = (z + offsetZ) * ratioGeneration;
 
             dist = Math.abs(rng.perlin2(offsettedZ / 400, offsettedX / 400));
 
             y = (rng.perlin2(offsettedX/ 100, offsettedZ/100) * rng.perlin2(offsettedX/ 66, offsettedZ/66) + rng.perlin2(offsettedX/ 33, offsettedZ/33)) * 2000;
             y += Math.pow(rng.perlin3(dist + y / 500, offsettedX/ 90, offsettedZ/1000) * rng.perlin2(offsettedX/ 760, offsettedZ/76) , 3) * 10000;
-            ndarrayMap2.set(x, z, y / Math.pow(2, 0.5 + dist));
+            typedArray[x + z * width] = y / Math.pow(2, 0.5 + dist);
         }
     }
 
-    var bufferGeometry = prepareBufferGeometry(ndarrayMap2.data, segmentSize, groundSegments);
+    var bufferGeometry = prepareBufferGeometry(typedArray, segmentSize, groundSegments);
 
     return {
         position: bufferGeometry.attributes.position.array,
